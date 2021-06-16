1d42c765ed95a1d2f26ffef010b5e464e2f67df4
Essi Suomalainen <essi.suomalainen@hyperisland.se>
2021-06-16 17:02:27 +0200

    added add task function

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,59 @@
 <template>
   <div class="container">
-    <Header title="Task tracker" />
-    <Tasks :tasks="tasks" /></div
+    <Header title="Task tracker" @toggle-add-task="toggleAddTask" />
+    <div v-if="showAddTask"><AddTask @add-task="addTask" /></div>
+
+    <Tasks
+      :tasks="tasks"
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+    /></div
 ></template>
 
 <script>
 import Header from './components/Header.vue';
 import Tasks from './components/Tasks.vue';
+import AddTask from './components/AddTask.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
     Tasks,
+    AddTask,
   },
   data() {
     return {
       tasks: [],
+      showAddTask: false,
     };
+  },
+  methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
+
+    deleteTask(id) {
+      if (confirm('Are you sure?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+    toggleReminder(id) {
+      console.log(id);
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
   },
   created() {
     this.tasks = [
       {
         id: 1,
-        text: 'teststse',
+        text: 'buy banana',
         day: '1 March 2021',
         reminder: true,
       },
@@ -31,7 +61,7 @@ export default {
         id: 2,
         text: 'teststse',
         day: '1 March 2021',
-        reminder: true,
+        reminder: false,
       },
       {
         id: 3,
